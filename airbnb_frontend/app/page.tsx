8d0dfd9f966ede3a23229a8c4f5e2fce1adada26
Mohamed Yehia <eng.essa85@gmail.com>
2024-11-11 15:20:18 +0200

    startup files

---
+++ airbnb_frontend/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="m-auto max-w-[400px] text-center bg-slate-400 mt-[100px] text-lg">
+      <h1>Welcome in airbnb project</h1>
+    </div>
+  );
+}
